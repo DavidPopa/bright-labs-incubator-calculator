ce24c02257e74d490db68f795d2295a2c02073a5
DavidPopa <davidpopa843@gmail.com>
2025-05-12 15:48:36 +0300

    fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Startup Cost Calculator",
-  description: "Startup Cost Calculator",
+  description:
+    "A tool to estimate and analyze the monthly expenses for startups, helping entrepreneurs plan their financial strategy effectively.",
 };
 
 export default function RootLayout({
